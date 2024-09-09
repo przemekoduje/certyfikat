ee6a63129547f67c1120ca17e0cd894431823734
przemokoduje <145247252+przemekoduje@users.noreply.github.com>
2024-09-09 14:32:31 +0200

    Opis zmian, które wprowadziłeś

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom"
 import AppComponent from './appComponent/AppComponent';
+import Navbar from '../src/components/navbar/Navbar';
+import Footer from '../src/components/footer/Footer';
+import HomePage from '../src/pages/homePage/HomePage';
+import RegisterPage from '../src/pages/registerPage/RegisterPage';
+import LoginPage from '../src/pages/loginPage/LoginPage';
+import FormPage from '../src/pages/formPage/FormPage'
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      
+  ]
+  },
+  {
+    path: "/form",
+    // element: <FormPage />
+    element: <AppComponent />
+  },
+  
+
+]);
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <AppComponent/>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
