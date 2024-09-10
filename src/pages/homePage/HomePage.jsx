410da2bb0a79c3fb738e6e98144f7d577d1e9b63
Przemysław Rakotny <przemyslawrakotny@MacBook-Air-Przemysaw.local>
2024-09-10 06:26:51 +0200

    Opis zmian, które wprowadziłeś

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./homePage.scss";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="homepage">
+      <div className="background">
+        <img className="homeImage" src="/images/design6_3_2.png" alt="" />
+      </div>
+      <div className="textButton">
+        <div className="textContainer">
+          <h1>Świadectwo na klik</h1>
+          <span>
+            Pobierz świadectwo na klik i wspólnie zadbajmy o środowisko!
+            <br />
+            Od każdego wystawionego dokumentu przekazujemy <span className="highlight">10 zł na sadzenie drzew</span>, łącząc energooszczędność z realnym wsparciem dla natury.
+          </span>
+        </div>
+        <button>Zróbmy to</button>
+      </div>
+    </div>
+  );
 }
