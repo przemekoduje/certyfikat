2ea7f0050b61774a04b0cf8ff0ef8957db529472
Przemysław Rakotny <przemyslawrakotny@MacBook-Air-Przemysaw.local>
2024-09-13 06:21:36 +0200

    Opis wprowadzonych zmian

--- src/components/home2/Home2.jsx
+++ src/components/home2/Home2.jsx
@@ -1,123 +1,132 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion"; // Importujemy motion z framer-motion
 import "./home2.scss";
-import Comix from "../comix/Comix";
 
-
-const images = [
-  "/images/Tło usunięte (scene1).png",
-  "/images/Tło usunięte (scene2).png",
-  "/images/Tło usunięte (scene3).png",
-  "/images/Tło usunięte (scene4).png",
-  "/images/Tło usunięte (scene5).png",
-  "/images/Tło usunięte (scene6).png",
+// Karty z obrazami
+const cards = [
+  { id: 1, imageUrl: "/images/Tło usunięte (scene1).png" },
+  { id: 2, imageUrl: "/images/Tło usunięte (scene2).png" },
+  { id: 3, imageUrl: "/images/Tło usunięte (scene3).png" },
+  { id: 4, imageUrl: "/images/Tło usunięte (scene4).png" },
 ];
-
-const head = [
-  "Czy Twój dom jest energooszczędny? ",
-  "Pierwszy krok do oszczędności.",
-  "Zoptymalizuj swój dom.",
-  "Komfort i oszczędność.",
-  "Świadectwo to krok ku lepszemu środowisku.",
-  "Świadectwo to inwestycja w naturę.",
-];
-
-const opis = [
-  "Świadectwo charakterystyki energetycznej pomaga odpowiedzieć na to pytanie.",
-  "Z pomocą specjalisty możesz dowiedzieć się, jak poprawić efektywność energetyczną swojego domu.",
-  "Świadectwo energetyczne wskazuje, gdzie Twój dom może zaoszczędzić energię – od izolacji po lepsze okna.",
-  "Lepsza efektywność energetyczna to niższe rachunki, większy komfort życia i przyjazność dla natury.",
-  "Dzięki świadectwom energetycznym zmniejszamy nasz ślad węglowy i dbamy o przyszłość planety.",
-  "Z każdym świadectwem sadzimy drzewa, wspólnie tworząc zdrowszą przyszłość dla nas wszystkich.",
-];
-
 
 export default function Home2() {
-  const comixContainerRef1 = useRef(null);
-  const comixContainerRef2 = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [zIndexes, setZIndexes] = useState(
+    cards.map((_, index) => cards.length - index)
+  );
 
-  useEffect(() => {
-    const animateComixItems = (comixItems, delayMultiplier = 0) => {
-      // Intersection Observer callback
-      const handleIntersection = (entries) => {
-        entries.forEach((entry) => {
-          const index = Array.from(comixItems).indexOf(entry.target); // Pobranie indeksu elementu
-          const delay = index * 0.2 + delayMultiplier; // Opóźnienie w sekundach z dodatkowym opóźnieniem dla 4-6
+  const handleSwipe = (direction) => {
+    const newZIndexes = [...zIndexes];
 
-          if (entry.isIntersecting) {
-            // Element wchodzi do widoku: rozpocznij animację z opóźnieniem
-            entry.target.style.transitionDelay = `${delay}s`;
-            entry.target.style.transform = "translateY(0px)";
-            entry.target.style.opacity = "1";
-          } else {
-            // Element opuszcza widok: resetuj do pierwotnej pozycji bez opóźnienia
-            entry.target.style.transitionDelay = "0s";
-            entry.target.style.transform = "translateY(200px)";
-            entry.target.style.opacity = "0";
-          }
-        });
-      };
-
-      // Ustawienia Intersection Observer
-      const observer = new IntersectionObserver(handleIntersection, {
-        threshold: 0.5, // Procent widoczności elementu wymagany do aktywacji
+    if (direction === "right") {
+      cards.forEach((_, index) => {
+        if (index === currentIndex) {
+          newZIndexes[index] -= 1; // active card z-index -1
+        } else if (index < currentIndex) {
+          newZIndexes[index] -= 1; // left z-index -1
+        } else {
+          newZIndexes[index] += 1; // stack z-index +1
+        }
       });
-
-      // Obserwowanie każdego elementu comix-item
-      Array.from(comixItems).forEach((item) => {
-        observer.observe(item);
+      setCurrentIndex((prevIndex) =>
+        prevIndex < cards.length - 1 ? prevIndex + 1 : prevIndex
+      );
+    } else if (direction === "left") {
+      cards.forEach((_, index) => {
+        if (index === currentIndex) {
+          newZIndexes[index] -= 1; // active card z-index -1
+        } else if (index < currentIndex) {
+          newZIndexes[index] += 1; // left z-index +1
+        } else {
+          newZIndexes[index] -= 1; // stack z-index -1
+        }
       });
+      setCurrentIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : prevIndex
+      );
+    }
 
-      // Czyszczenie obserwatora po demontażu komponentu
-      return () => {
-        Array.from(comixItems).forEach((item) => {
-          observer.unobserve(item);
-        });
+    setZIndexes(newZIndexes);
+  };
+
+  const getCardStyle = (index) => {
+    const distance = Math.abs(index - currentIndex);
+    const offset = distance * 50; // Stała wartość dla przesunięcia
+    const scale = 1 - distance * 0.07; // Stała wartość dla zmniejszania skali
+
+    if (index === currentIndex) {
+      return {
+        zIndex: zIndexes[index],
       };
-    };
-
-    // Animowanie pierwszego rzędu (comix 1-3)
-    const comixItems1 = comixContainerRef1.current.children;
-    animateComixItems(comixItems1);
-
-    // Animowanie drugiego rzędu (comix 4-6) z dodatkowym opóźnieniem
-    const comixItems2 = comixContainerRef2.current.children;
-    const additionalDelay = 0.6; // Dodatkowe opóźnienie dla comix 4-6 po animacji comix 1-3
-    animateComixItems(comixItems2, additionalDelay);
-  }, []);
+    } else if (index < currentIndex) {
+      return {
+        zIndex: zIndexes[index],
+      };
+    } else {
+      return {
+        zIndex: zIndexes[index],
+      };
+    }
+  };
 
   return (
     <div className="home2-container">
-      <div className="display">
-        <h1>Na co komu ten cały kłopot?</h1>
+      <div className="card-swipe-container">
+        {cards.map((card, index) => (
+          <motion.div
+            key={card.id}
+            className={`card ${
+              index === currentIndex
+                ? "active"
+                : index < currentIndex
+                ? "left"
+                : "card-stack"
+            }`}
+            style={getCardStyle(index)}
+            
+            // Dodajemy animacje
+            animate={{
+              x: index === currentIndex 
+                ? 0 
+                : index < currentIndex 
+                ? -35 - (Math.abs(currentIndex - index) * 50) // Przesuwanie na lewo
+                : 35 + (Math.abs(currentIndex - index) * 50), // Przesuwanie na prawo
+              scale: index === currentIndex 
+                ? 1 
+                : 1 - (Math.abs(currentIndex - index) * 0.07), // Skala
+            }}
+
+            // Definiujemy animację przejścia
+            transition={{
+              type: "spring", // Zastosowanie sprężyny
+              stiffness: 300, // Im wyższa wartość, tym szybciej karta wróci na miejsce
+              damping: 30, // Kontroluje "drgania" przy animacji
+            }}
+          >
+            <img src={card.imageUrl} alt={`Card ${card.id}`} className="card-img" />
+          </motion.div>
+        ))}
       </div>
 
-      <span className="info_h2 lato-regular">Świadectwo charakterystyki energetycznej to nie zbędna formalność, ale klucz do oszczędności i wyższej wartości Twojej nieruchomości. Dzięki niemu wiesz, gdzie Twój dom traci energię i jakie kroki podjąć, by to zmienić – od poprawy izolacji po wymianę okien. Lepsza efektywność energetyczna to nie tylko niższe rachunki, ale i wyższa wartość rynkowa budynku. Inwestycja w świadectwo to pierwszy krok do poprawy komfortu życia, zwiększenia atrakcyjności nieruchomości na rynku oraz wsparcia dla środowiska, bo każdy z nas może działać na rzecz redukcji emisji CO₂.</span>
-
-      <div className="images-cont_h2">
-        <div ref={comixContainerRef1} className="comix-container cont1">
-          {images.slice(0, 3).map((img, index) => (
-            <div className="comix-item" key={index}>
-              <Comix
-                image={img}
-                head={head[index]}
-                opis={opis[index]}
-              />
-            </div>
-          ))}
-        </div>
-        <div ref={comixContainerRef2} className="comix-container cont2">
-          {images.slice(3, 6).map((img, index) => (
-            <div className="comix-item" key={index + 3}>
-              <Comix
-                image={img}
-                head={head[index + 3]}
-                opis={opis[index + 3]}
-              />
-            </div>
-          ))}
-        </div>
+      <div className="controls">
+      <button
+          onClick={() => handleSwipe("left")}
+          style={{
+            visibility: currentIndex === 0 ? "hidden" : "visible", // Ukrywamy, ale zachowujemy miejsce
+          }}
+        >
+          &lt;
+        </button>
+        <button
+          onClick={() => handleSwipe("right")}
+          style={{
+            visibility: currentIndex === cards.length - 1 ? "hidden" : "visible", // Ukrywamy, ale zachowujemy miejsce
+          }}
+        >
+          &gt;
+        </button>
       </div>
-
     </div>
   );
 }
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./homePage.scss";
 import Home1 from "../../components/home1/Home1";
 import Home2 from "../../components/home2/Home2";
+import Home3 from "../../components/home3/Home3";
 
 export default function HomePage() {
   return (
     <div className="homepage">
       <Home1/>
       <Home2/>
+      <Home3/>
     </div>
   );
 }
